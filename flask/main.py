8d41c4c0f074b72461c700e69906fddd81943806
xiaogua255 <xiaogua255@outlook.com>
2016-11-23 20:15:45 +0800

    更改端口

--- flask/main.py
+++ flask/main.py
@@ -45,7 +45,7 @@ if __name__ == '__main__':
     config = dict(
         debug=True,
         host='0.0.0.0',
-        port=3000,
+        port=3333,
         threaded=True,
     )
     app.run(**config)
